53d7f3a3f19e3331f0607ff2693df60002744dcb
Mattia Baroni <92336539+bar0x@users.noreply.github.com>
2022-03-11 09:34:19 +0100

    Add files via upload

---
+++ lcd.ino
@@ -0,0 +1,18 @@
+#include <LiquidCrystal.h>
+//inizializzo il pin RS, E, D4; D5, D6, D7
+LiquidCrystal Davide(12, 11, 8, 7, 6, 5);
+
+
+
+
+void setup() {
+  Serial.begin(9600);
+  Davide.begin(16, 2); //grandezza display (16 colonne e 2 righe)
+}
+
+void loop() {
+  // posiziono il cursore nella seconda riga 5 colonna
+  Davide.setCursor(2,5);
+  Davide.print("ciao, Davide");
+
+}
